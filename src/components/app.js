b4aaa9c3f4aa27aa9184e1815177a5371c59dc81
Vitaly <vitalyrgame@gmail.com>
2020-10-16 13:34:46 +0300

    no1

---
+++ src/components/app.js
@@ -0,0 +1,26 @@
+import React from "react";
+import NavBar from "./navbar";
+
+
+
+
+const App = () => {
+    return (
+        <>
+            <NavBar></NavBar>
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default App
